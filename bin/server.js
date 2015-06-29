12275b550185bcc2dbe98416440f56bc6a21ad7f
Reto Kaiser <reto@cargomedia.ch>
2015-06-29 23:53:28 +0200

    Add WS server

---
+++ bin/server.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var websocketServer = require('websocket').server;
+var fs = require('fs');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+var staticPort = 1234;
+connect().use(serveStatic(__dirname + '/static')).listen(staticPort, function() {
+  console.log('HTTP server listening on port ' + staticPort);
+});
+
+var websocketPort = 8090;
+var http = http.createServer(function(request, response) { });
+http.listen(websocketPort, function() {
+  console.log('WebSocket server listening on port ' + websocketPort);
+});
+
+var websocket = new websocketServer({ httpServer: http });
+websocket.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+  connection.binaryType = "arraybuffer";
+  console.log('WS connect');
+
+  var data = fs.readFileSync('static/video/test.webm');
+  connection.sendBytes(data);
+
+  connection.on('close', function(connection) {
+    console.log('WS disconnect');
+  });
+});
